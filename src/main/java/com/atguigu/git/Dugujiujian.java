6570927d57ac6e25910c1526d38e8927e699ecb4
felix <fxf_java@163.com>
2020-11-04 15:57:02 +0800

    buqun update1

--- src/main/java/com/atguigu/git/Dugujiujian.java
+++ src/main/java/com/atguigu/git/Dugujiujian.java
@@ -7,6 +7,6 @@ package com.atguigu.git;
  */
 public class Dugujiujian {
     public static void main(String[] args) {
-        System.out.println("紫霞神功");
+        System.out.println("紫霞神功111");
     }
 }
